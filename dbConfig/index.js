d76fe23dacfa6748adc83789073606422a160651
Pratyush Birole <birole.pratyush@gmail.com>
2024-10-10 00:58:55 +0530

    MongoDB Server Config

---
+++ dbConfig/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,       // Optional: Check if needed based on Mongoose version
+            useUnifiedTopology: true,    // Optional: Check if needed based on Mongoose version
+        });
+        console.log("Connected to DB Successfully");
+    } catch (error) {
+        console.error("Error connecting to DB:", error);
+    }
+};
+
+// Optionally, export the connection function if you need to use it elsewhere
+export default dbConnection;
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+
+
+//Security Packages
+import helment from'helmet'
+import dbConnection from "./dbConfig/index"
+
+dotenv.config
+
+const app = express()
+
+const PORT = process.env.PORT || 8800;
+dbConnection();
+
+app.use(helment());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json({limit : "10mb"}));
+app.use(express.urlencoded({extended:true}))
+
+
+app.use(morgan("dev"));
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on Port: ${PORT}` );
+})
+
+
