10909a54245195c380b793779e96051b979b5545
Pratyush Birole <birole.pratyush@gmail.com>
2024-10-11 20:53:47 +0530

    Setting up controller and JWT password

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,3 +1,28 @@
 import Users from "../models/userModel";
 
 
+export const register = async(req, res, next)=>{
+    const {firstName , lastName , email , password}= req.body;
+
+
+
+    //validate fields
+    if(!firstName || lastName || email || password){
+        next("Provide Required Fields!")
+        return ;
+    }try{
+        const userExist = await Users.findOne({email});
+
+        if(userExist){
+            next("User already Exists");
+            return;
+        }
+
+        const hashedPassword = await hashedPassword(password);
+
+    }catch(error){
+     console.log(error);
+     res.status(404).json({message:error.message});   
+    }
+    
+}
