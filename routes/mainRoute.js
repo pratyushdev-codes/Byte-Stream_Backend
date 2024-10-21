a031a2ffd7b7eab44d5a330dd5ce206a7fe6cb49
Pratyush Birole <birole.pratyush@gmail.com>
2024-10-22 02:18:48 +0530

    Fixed Post routes, Added create post and fetchpost fns to backend | Fixed some frontend

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from './authRoutes.js'
+import postRoutes from './postRoutes.js'
 import   userRoutes from './userRoutes.js'
 
 
@@ -8,6 +9,7 @@ const router = express.Router();
 
 router.use("/auth", authRoutes) //auth/register
 router.use("/users", userRoutes);
+router.use('/posts', postRoutes);
 
 
 export default router
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import userAuth from '../middleware/authMiddleware.js';
+import {createPost} from'../Controllers/postController.js'
+
 
 const router = express.Router();
 
 //creating post :-
 router.post("/create-post",userAuth,createPost);
+
+
+//fetch post
+router.get("/",userAuth, getPosts);
+
+export default router
